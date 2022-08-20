71474c50a6dac3d11725360c4d65f1380d21175a
EvgiSs <neskisun@gmail.com>
2022-08-20 03:09:47 +0300

    all page

--- src/app.js
+++ src/app.js
@@ -1,16 +1,46 @@
-// Swiper
-const swiper = new Swiper(".swiper", {
-  loop: true,
+$(document).ready(function () {
+  // Плавная прокрутка страницы при клике на кнопку
+  $(".order-link").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      1000
+    );
+  });
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  // Swiper
+  const swiper = new Swiper(".swiper", {
+    loop: true,
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
 
-  grabCursor: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    grabCursor: true,
+  });
+
+  // Timer
+  $(".timer").startTimer({
+    loop: false,
+    loopInterval: 1,
+    onComplete: function (element) {
+      element.css("display", "none");
+      alert("Время вышло.");
+    },
+    classNames: {
+      hours: "hours",
+      minutes: "minutes",
+      seconds: "seconds",
+      clearDiv: "clearDiv",
+      timeout: "timeout",
+    },
+  });
 });
